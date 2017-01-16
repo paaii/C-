fc89888d833276d58cd582a40c75c8065d1f11e6
paaii <paakki91@hotmail.com>
2017-01-16 13:01:20 +0200

    Add files via upload

---
+++ 9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace teht9
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int summa = 0, luku = 0;
+			while (true) 
+			{
+				Console.WriteLine ("Anna luku ");
+				luku = int.Parse (Console.ReadLine ());
+
+				if (luku == 0)
+					break;
+				else
+					summa += luku;
+			}
+			Console.WriteLine ("lukujen summa " + summa);
+			Console.ReadKey ();
+	}
+}
+}
