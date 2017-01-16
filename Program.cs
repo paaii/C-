150b045421f5999433867b2de179118b896fe17b
paaii <paakki91@hotmail.com>
2017-01-16 14:34:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("anna luku");
+            int luku;
+            luku = int.Parse(Console.ReadLine());
+            if (luku < 18)
+                Console.WriteLine("alaikainen");
+            if (luku == 18 || luku <= 56)
+                Console.WriteLine("aikuinen");
+            else Console.WriteLine("seniori");
+            Console.ReadLine();
+        }
+    }
+}
